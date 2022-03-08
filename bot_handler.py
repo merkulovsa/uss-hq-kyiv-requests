05a7d78a2ad895f8b97f73fa200f376d9571f5a9
Serhii Merkulov <smerkulov@c.snap.com>
2022-03-08 14:04:15 +0200

    added temp StateMachine behaviour

--- bot_handler.py
+++ bot_handler.py
@@ -1,3 +1,4 @@
+from distutils.command.build import build
 import os
 import time
 import logging
@@ -5,11 +6,56 @@ import telegram
 import wget
 from telegram.error import NetworkError, Unauthorized
 from time import sleep
+from state_machine import StateMachine, State
 
 TOKEN = None
+REQUEST_BUTTON = 'Створити запит'
+NUMBER_BUTTON = 'Змінити номер частини'
 update_id = None
-handlers = {}
 
+def get_keyboard_markup(*button_names):
+    buttons = [
+        [telegram.KeyboardButton(x) for x in button_names]
+    ]
+    return telegram.ReplyKeyboardMarkup(buttons)
+
+class HandlerState(State):
+    def on_bot_update(self, update):
+        return
+
+class StartState(HandlerState):
+    name = 'StartState'
+
+    def on_bot_update(self, update):
+        update.message.reply_text('Hello world!')
+        self._next_state = NumberState.name
+
+class NumberState(HandlerState):
+    name = 'NumberState'
+
+    def on_bot_update(self, update):
+        if update.message.text:
+            chat_id = str(update.message.chat.id)
+            user_data[chat_id]['number'] = update.message.text
+
+            update.message.reply_text('_number_msg_text', reply_markup=get_keyboard_markup(REQUEST_BUTTON, NUMBER_BUTTON))            
+
+            self._next_state = IdleState.name
+
+class IdleState(HandlerState):
+    name = 'IdleState'
+
+    def on_bot_update(self, update):
+        if update.message.text == REQUEST_BUTTON:
+            self._next_state = RequestState.name
+        elif update.message.text == NUMBER_BUTTON:
+            self._next_state = NumberState.name
+
+class RequestState(HandlerState):
+    name = 'RequestState'
+
+machines = {}
+user_data = {}
 
 def start():
     global update_id
@@ -41,21 +87,22 @@ def __process(bot):
         update_id = update.update_id + 1
 
         if update.message:
-            if update.message.text in handlers:
-                handlers[update.message.text](update)
+            chat_id = str(update.message.chat.id)
 
-def __start_command(update):
-    buttons = [
-        [
-            telegram.KeyboardButton('Створити запит')
-        ]
-    ]
-    reply_markup = telegram.ReplyKeyboardMarkup(buttons)
+            if chat_id not in machines:
+                __init_machine(chat_id)
+                user_data[chat_id] = {}
 
-    update.message.reply_text('Hello world!', reply_markup=reply_markup)
+            machines[chat_id].get_active_state().on_bot_update(update)
+            machines[chat_id].update()
 
-def __request_command(update):
-    update.message.reply_text('Request received!')
+def __init_machine(chat_id):
+    machine = StateMachine()
+    machine.add(StartState(machine))
+    machine.add(NumberState(machine))
+    machine.add(IdleState(machine))
+    machine.add(RequestState(machine))
 
-handlers['/start'] = __start_command
-handlers['Створити запит'] = __request_command
+    machine.change(StartState.name)
+    
+    machines[chat_id] = machine
