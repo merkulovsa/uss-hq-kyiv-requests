4c7336f0841ae32de0112cb0827816d0c8ec18ee
kulturkamp <dgtrv123@gmail.com>
2022-03-08 16:56:12 +0200

    completed basic flow

--- bot_handler.py
+++ bot_handler.py
@@ -1,23 +1,28 @@
 from distutils.command.build import build
-import os
-import time
 import logging
 import telegram
-import wget
 from telegram.error import NetworkError, Unauthorized
+from telegram import KeyboardButton, ReplyKeyboardMarkup
 from time import sleep
 from state_machine import StateMachine, State
 
+NEW_LINE = '\n'
+
 TOKEN = None
-REQUEST_BUTTON = 'Створити запит'
+
+REQUEST_BUTTON = 'Створити замовлення'
 NUMBER_BUTTON = 'Змінити номер частини'
+COMPLETE_ORDER_BUTTON = 'Завершити замовлення'
+CONFIRM_ORDER_BUTTON = 'Підтвердити замовлення'
+
 update_id = None
+order = []
 
 def get_keyboard_markup(*button_names):
     buttons = [
-        [telegram.KeyboardButton(x) for x in button_names]
+        [KeyboardButton(x) for x in button_names]
     ]
-    return telegram.ReplyKeyboardMarkup(buttons)
+    return ReplyKeyboardMarkup(buttons)
 
 class HandlerState(State):
     def on_bot_update(self, update):
@@ -36,9 +41,12 @@ class NumberState(HandlerState):
     def on_bot_update(self, update):
         if update.message.text:
             chat_id = str(update.message.chat.id)
-            user_data[chat_id]['number'] = update.message.text
+            user_data[chat_id]['number'] = update.message.text #why dict of dicts
 
-            update.message.reply_text('_number_msg_text', reply_markup=get_keyboard_markup(REQUEST_BUTTON, NUMBER_BUTTON))            
+            update.message.reply_text(
+                f'Номер вашої частини: {update.message.text}', 
+                reply_markup=get_keyboard_markup(REQUEST_BUTTON, NUMBER_BUTTON)
+            )            
 
             self._next_state = IdleState.name
 
@@ -47,6 +55,10 @@ class IdleState(HandlerState):
 
     def on_bot_update(self, update):
         if update.message.text == REQUEST_BUTTON:
+            update.message.reply_text(
+                'Створіть ваше замовлення.\nВажливо! Кожне повідомлення - окремий допис.',
+                reply_markup=get_keyboard_markup(COMPLETE_ORDER_BUTTON)
+            )
             self._next_state = RequestState.name
         elif update.message.text == NUMBER_BUTTON:
             self._next_state = NumberState.name
@@ -54,6 +66,35 @@ class IdleState(HandlerState):
 class RequestState(HandlerState):
     name = 'RequestState'
 
+    def on_bot_update(self, update):
+        if update.message.text != COMPLETE_ORDER_BUTTON:
+            order.append(str(update.message.text))
+            update.message.reply_text(f'Ви додоали до замовлення:\n{update.message.text}')
+            print(f'Current order: {order}')
+        elif update.message.text == COMPLETE_ORDER_BUTTON:
+        
+
+            update.message.reply_text(
+                f'Ваше замовлення:{NEW_LINE}{NEW_LINE.join(str(item) for item in order)}',
+                reply_markup=get_keyboard_markup(CONFIRM_ORDER_BUTTON)
+            )
+            
+            self._next_state = CompleteOrderState.name
+
+
+class CompleteOrderState(HandlerState):
+    name = 'CompleteOrderState'
+
+    def on_bot_update(self, update):  
+        if update.message.text == CONFIRM_ORDER_BUTTON:
+            update.message.reply_text('Замовлення підтверджено')
+            #further order processing
+            #...
+            #...
+            #...
+            self._next_state = IdleState.name
+
+            
 machines = {}
 user_data = {}
 
@@ -102,6 +143,7 @@ def __init_machine(chat_id):
     machine.add(NumberState(machine))
     machine.add(IdleState(machine))
     machine.add(RequestState(machine))
+    machine.add(CompleteOrderState(machine))
 
     machine.change(StartState.name)
     
