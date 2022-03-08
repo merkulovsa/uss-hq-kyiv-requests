d8d5b32b44f47b465b39ad11490410cb73b9f791
Serhii Merkulov <smerkulov@c.snap.com>
2022-03-08 17:41:01 +0200

    refactored HandlerState + fixed user_data

--- bot_handler.py
+++ bot_handler.py
@@ -1,4 +1,3 @@
-from distutils.command.build import build
 import logging
 import telegram
 from telegram.error import NetworkError, Unauthorized
@@ -8,57 +7,69 @@ from state_machine import StateMachine, State
 
 NEW_LINE = '\n'
 
-TOKEN = None
-
 REQUEST_BUTTON = 'Створити замовлення'
 NUMBER_BUTTON = 'Змінити номер частини'
 COMPLETE_ORDER_BUTTON = 'Завершити замовлення'
 CONFIRM_ORDER_BUTTON = 'Підтвердити замовлення'
+CANCEL_ORDER_BUTTON = 'Скасувати замовлення'
 
 update_id = None
-order = []
-
-def get_keyboard_markup(*button_names):
-    buttons = [
-        [KeyboardButton(x) for x in button_names]
-    ]
-    return ReplyKeyboardMarkup(buttons)
+bot = None
+machines = {}
+chat_ids = {}
+user_datas = {}
 
 class HandlerState(State):
+    def get_chat_id(self):
+        return chat_ids[self._state_machine]
+
+    def get_user_data(self):
+        return user_datas[self.get_chat_id()]
+
     def on_bot_update(self, update):
-        return
+        pass
+
+    def send_message(self, text, *button_names):
+        global bot
+
+        buttons = [
+            [KeyboardButton(x) for x in button_names]
+        ]
+        bot.send_message(chat_id=self.get_chat_id(), text=text, reply_markup=ReplyKeyboardMarkup(buttons, resize_keyboard=True))
+
 
 class StartState(HandlerState):
     name = 'StartState'
 
     def on_bot_update(self, update):
-        update.message.reply_text('Hello world!')
         self._next_state = NumberState.name
 
 class NumberState(HandlerState):
     name = 'NumberState'
 
+    def enter(self):
+        super().enter()
+
+        self.send_message('Введіть номер частини:')
+
     def on_bot_update(self, update):
         if update.message.text:
-            chat_id = str(update.message.chat.id)
-            user_data[chat_id]['number'] = update.message.text #why dict of dicts
-
-            update.message.reply_text(
-                f'Номер вашої частини: {update.message.text}', 
-                reply_markup=get_keyboard_markup(REQUEST_BUTTON, NUMBER_BUTTON)
-            )            
-
+            self.get_user_data()['number'] = update.message.text
             self._next_state = IdleState.name
 
 class IdleState(HandlerState):
     name = 'IdleState'
 
+    def enter(self):
+        super().enter()
+
+        number = self.get_user_data()['number']
+
+        self.send_message(f'Номер вашої частини: {number}')
+        self.send_message('Виберіть наступний крок:', REQUEST_BUTTON, NUMBER_BUTTON)
+
     def on_bot_update(self, update):
         if update.message.text == REQUEST_BUTTON:
-            update.message.reply_text(
-                'Створіть ваше замовлення.\nВажливо! Кожне повідомлення - окремий допис.',
-                reply_markup=get_keyboard_markup(COMPLETE_ORDER_BUTTON)
-            )
             self._next_state = RequestState.name
         elif update.message.text == NUMBER_BUTTON:
             self._next_state = NumberState.name
@@ -66,44 +77,50 @@ class IdleState(HandlerState):
 class RequestState(HandlerState):
     name = 'RequestState'
 
+    def enter(self):
+        super().enter()
+
+        self.get_user_data()['order'] = []
+        self.send_message('Створіть ваше замовлення.\nВажливо! Кожне повідомлення - окремий допис.', COMPLETE_ORDER_BUTTON)
+
     def on_bot_update(self, update):
         if update.message.text != COMPLETE_ORDER_BUTTON:
-            order.append(str(update.message.text))
-            update.message.reply_text(f'Ви додоали до замовлення:\n{update.message.text}')
-            print(f'Current order: {order}')
-        elif update.message.text == COMPLETE_ORDER_BUTTON:
-        
-
-            update.message.reply_text(
-                f'Ваше замовлення:{NEW_LINE}{NEW_LINE.join(str(item) for item in order)}',
-                reply_markup=get_keyboard_markup(CONFIRM_ORDER_BUTTON)
-            )
+            self.get_user_data()['order'].append(str(update.message.text))
+            self.send_message(f'Ви додали до замовлення:\n{update.message.text}', COMPLETE_ORDER_BUTTON)
             
+        elif update.message.text == COMPLETE_ORDER_BUTTON:
             self._next_state = CompleteOrderState.name
 
 
 class CompleteOrderState(HandlerState):
     name = 'CompleteOrderState'
 
+    def enter(self):
+        super().enter()
+
+        order = self.get_user_data()['order']
+
+        self.send_message(f'Ваше замовлення:{NEW_LINE}{NEW_LINE.join(str(item) for item in order)}', CONFIRM_ORDER_BUTTON, CANCEL_ORDER_BUTTON)
+
     def on_bot_update(self, update):  
         if update.message.text == CONFIRM_ORDER_BUTTON:
-            update.message.reply_text('Замовлення підтверджено')
+            self.send_message('Замовлення підтверджено')
             #further order processing
             #...
             #...
             #...
-            self._next_state = IdleState.name
-
-            
-machines = {}
-user_data = {}
+        elif update.message.text == CANCEL_ORDER_BUTTON:
+            self.send_message('Замовлення скасовано')
+        
+        self._next_state = IdleState.name
 
 def start():
     global update_id
+    global bot
 
     with open('TOKEN', 'r') as f:
-        TOKEN = f.read()
-        bot = telegram.Bot(TOKEN)
+        token = f.read()
+        bot = telegram.Bot(token)
 
         try:
             update_id = bot.get_updates()[0].update_id
@@ -132,7 +149,7 @@ def __process(bot):
 
             if chat_id not in machines:
                 __init_machine(chat_id)
-                user_data[chat_id] = {}
+                user_datas[chat_id] = {}
 
             machines[chat_id].get_active_state().on_bot_update(update)
             machines[chat_id].update()
@@ -148,3 +165,4 @@ def __init_machine(chat_id):
     machine.change(StartState.name)
     
     machines[chat_id] = machine
+    chat_ids[machine] = chat_id
